21f4548405f3371ebe7df88cd2f37be5d4900773
Jamie Kitson <github@kitten-x.com>
2026-01-26 22:42:49 +0000

    Adding python files

---
+++ fullFrame/imagesd.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+import subprocess
+import time
+import os
+
+from io import BytesIO
+from PIL import Image
+from inky.auto import auto
+from gpiozero import Button
+from smbus2 import SMBus, i2c_msg
+from enum import IntEnum
+
+url = "http://192.168.1.4/py/pics3.cgi"
+BUTTON_PIN = 5
+WAIT_SECONDS = 30
+
+class I2CCommand(IntEnum):
+    READ_BUTTON = 0x01
+    READ_VOLTAGE = 0x02
+    SHUTDOWN = 0x10
+
+def i2c(cmd):
+    try:
+        with SMBus(1) as bus:
+            bus.write_byte(0x12, cmd)
+            msg = i2c_msg.read(0x12, 1)
+            bus.i2c_rdwr(msg)
+            data = list(msg)
+            return data[0]
+    except Exception as e:
+        print(f"I2C communication error: {e}")
+        return -1
+
+def read_voltage():
+    voltage = i2c(I2CCommand.READ_VOLTAGE)
+    return voltage / 25  # Convert to volts
+
+def arduino_button_pressed():
+    button_state = i2c(I2CCommand.READ_BUTTON)
+    return button_state == 1
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--saturation", "-s", type=float, default=0.5, help="Colour palette saturation")
+
+inky = auto()
+
+args, _ = parser.parse_known_args()
+
+voltage = read_voltage()
+
+response = requests.get(url + f"?v={voltage:.2f}")
+img = Image.open(BytesIO(response.content))
+
+img.save("/home/jamie/inky/examples/spectra6/image.jpg")
+resizedimage = img.resize(inky.resolution)
+
+try:
+    inky.set_image(resizedimage, saturation=args.saturation)
+except TypeError:
+    inky.set_image(resizedimage)
+
+inky.show()
+
+print(f"Waiting {WAIT_SECONDS} seconds for input...")
+
+start = time.time()
+
+pi_button = Button(BUTTON_PIN) 
+
+while time.time() - start < WAIT_SECONDS:
+    if pi_button.is_pressed:
+        print("Shutdown cancelled")
+        exit(0)
+        break
+
+    if arduino_button_pressed():
+        print("Arduino button pressed: restarting script")
+        os.execv(__file__, ["python3", __file__]) # os.execv replaces the current process
+        break
+
+    time.sleep(0.1)
+
+print("No button press, shutting down")
+piState = i2c(I2CCommand.SHUTDOWN)
+print(f"I2C Shutdown command response: {piState}")
+subprocess.run(["sudo", "shutdown", "-h", "now"])
+
---
+++ fullFrame/pics3.cgi
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import os, random, cgi, sys, datetime
+from PIL import Image, ImageFilter
+from io import BytesIO
+
+IMAGE_FOLDER = "/srv/http/192.168.1.4/resized/"
+
+def crop(img, target_w, target_h):
+
+	w, h = img.size
+
+	left = (w - target_w) // 2
+	top = (h - target_h) // 2
+	right = left + target_w
+	bottom = top + target_h
+
+	return img.crop((left, top, right, bottom))
+
+
+try:
+    images = [f for f in os.listdir(IMAGE_FOLDER)
+        if f.lower().endswith((".jpg", ".jpeg", ".png"))]
+
+    if not images:
+        print("Content-Type: text/plain\n")
+        print("No images found")
+        sys.exit(0)
+
+    filename = random.choice(images)
+
+    v = cgi.FieldStorage().getvalue('v')
+    
+    with open('log.log', 'a') as file:
+        file.write(datetime.datetime.now().strftime('%x %X ') + f"{filename} {v} \n")
+
+    filepath = os.path.join(IMAGE_FOLDER, filename)
+
+    # Load image
+    img = Image.open(filepath)
+    
+    # for portraight pictures crop to a square using width as height
+    if img.height > img.width:
+        img = crop(img, img.width, img.width)
+        
+    # Define target size of final image, 4:3, maintain (new) height
+    target_h = img.height
+    target_w = int(4 * target_h / 3)
+
+    # if image is < 4:3 add blurred sides
+    if img.width / img.height < 4 / 3:
+   
+        # Create blurred background
+        bg = img.resize((target_w, target_h), Image.LANCZOS).filter(ImageFilter.GaussianBlur(50))
+
+        # Paste original image centered
+        bg.paste(img, ((target_w - img.width) // 2, 0))
+
+        img = bg
+
+    # if images is > 4:3 then crop left and right
+    else:
+
+        img = crop(img, target_w, target_h)
+
+    # scale to desired size
+    img = img.resize((1600, 1200), Image.LANCZOS)
+
+    buf = BytesIO()
+    img.save(buf, format="JPEG", quality=95)
+    buf.seek(0)
+
+    sys.stdout.buffer.write(b"Content-Type: image/jpeg\n\n")    
+    sys.stdout.flush()
+
+    sys.stdout.buffer.write(buf.read())
+
+except Exception as e:
+    print("Content-Type: text/plain\n")
+    print(f"Error: {e}")
+
