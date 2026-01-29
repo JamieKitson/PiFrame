56f309b2172b22942077399e0fcd08b77e3980aa
Jamie Kitson <github@kitten-x.com>
2026-01-29 12:48:34 +0000

    Remove Arduino RTC code

--- fullFrame/fullFrame.ino
+++ fullFrame/fullFrame.ino
@@ -1,5 +1,4 @@
 #include <Wire.h>
-//#include <RTClib.h>
 #include <LowPower.h>
 
 // ------------------------- Pin definitions -------------------------
@@ -9,8 +8,6 @@
 #define PIN_RTC_INT       2
 
 // ------------------------- Globals -------------------------
-//RTC_DS3231 rtc;
-
 volatile bool buttonIRQ = false;
 volatile bool rtcWake = false;
 
@@ -54,19 +51,7 @@ void turnPiOff() {
     digitalWrite(PIN_PI_POWER, LOW);
     piState = PI_OFF;    
 }
-/*
-// ------------------------- RTC alarm -------------------------
-#define SLEEP_MINUTES     1
 
-void setRTCAlarm24h() {
-    DateTime now = rtc.now();
-    DateTime wake = now + TimeSpan(0, 0, SLEEP_MINUTES, 0);
-    rtc.clearAlarm(1);
-    rtc.setAlarm1(wake, DS3231_A1_Date); // match date/time
-//    rtc.armAlarm(1, true);
-    rtc.clearAlarm(1); // clear previous flags
-}
-*/
 // ------------------------- I2C -------------------------
 #define I2C_ADDRESS 0x12
 #define SHUTDOWN_DELAY_SECS 10
@@ -150,43 +135,16 @@ void blink(int times, int delayMs = 100)
 // ------------------------- Setup -------------------------
 void setup() {
 
-        blink(1);
-
-//    delay(1000);
-
     pinMode(PIN_PI_POWER, OUTPUT);
     pinMode(PIN_BUTTON, INPUT_PULLUP);
     pinMode(PIN_RTC_INT, INPUT_PULLUP);
 
     attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), buttonISR, FALLING);
     attachInterrupt(digitalPinToInterrupt(PIN_RTC_INT), rtcISR, FALLING);
-/*
-    // initializing the rtc
-    while(!rtc.begin()) {
-        blink(1);
-    }
 
-    //we don't need the 32K Pin, so disable it
-    rtc.disable32K();
-
-    // set alarm 1, 2 flag to false (so alarm 1, 2 didn't happen so far)
-    // if not done, this easily leads to problems, as both register aren't reset on reboot/recompile
-    rtc.clearAlarm(1);
-    rtc.clearAlarm(2);
-
-    // stop oscillating signals at SQW Pin
-    // otherwise setAlarm1 will fail
-    rtc.writeSqwPinMode(DS3231_OFF);
-
-    // turn off alarm 2 (in case it isn't off already)
-    // again, this isn't done at reboot, so a previously set alarm could easily go overlooked
-    rtc.disableAlarm(2);
-
-    delay(1000);
-*/
-        blink(1);
+    blink(1);
     Wire.begin(I2C_ADDRESS);
-        blink(1);
+    blink(1);
     Wire.onReceive(onI2CReceive);
     Wire.onRequest(onI2CRequest);
 
@@ -219,22 +177,16 @@ void loop() {
 
         updateLed();                 // make sure LED is off
 
-//        setRTCAlarm24h();            // schedule next wake
-//        rtcWake = false;             // clear wake flag
-
         LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // sleep until RTC alarm
 
         return;                      // loop continues after wake
     }
 
+    // --- Handle waking Pi ---
     if (rtcWake) {
         rtcWake = false;
         turnPiOn();                 // ensure Pi is on after RTC wake
     }
-
-    // --- Optional: read battery voltage while Pi is on ---
-//    float batteryVoltage = readBatteryVoltage();
-    // Can be sent over I2C when Pi requests it
 
     delay(50); // small loop delay
 }
